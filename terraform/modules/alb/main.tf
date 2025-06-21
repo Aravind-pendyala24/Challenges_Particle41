220886978f1a17fe919032176c9290441817725d
Aravind Pendyala <80102707+Aravind-pendyala24@users.noreply.github.com>
2025-06-21 16:23:26 +0530

    Create main.tf

---
+++ terraform/modules/alb/main.tf
@@ -0,0 +1,11 @@
+resource "helm_release" "aws_lb_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+
+  set {
+    name  = "clusterName"
+    value = var.cluster_name
+  }
+}
